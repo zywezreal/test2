25314d177f43e452e510c37a936c11ed20baf917
ZhangYuwei <466299918@qq.com>
2018-04-11 17:38:18 +0800

    Initial commit

---
+++ src/hello.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class hello {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        int zyw=1;
+        System.out.println(zyw);
+        ArrayList<Integer> list=new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(i);
+        }
+        System.out.println(list.toString());
+    }
+}
